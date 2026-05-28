692fa4d7a2392f9ca540b2afef5fdb8b0c7c4d4c
DiegoGonzalezVillar <gonzalezvillardiego@gmail.com>
2026-05-28 10:06:23 -0300

    Demo cliente primera etapa

--- backend/src/db.js
+++ backend/src/db.js
@@ -4,12 +4,24 @@ dns.setDefaultResultOrder("ipv4first");
 import dotenv from "dotenv";
 dotenv.config();
 
-import pkg from "pg";
-const { Pool } = pkg;
+import pg from "pg";
+
+const { Pool } = pg;
+
+console.log("DATABASE_URL =", process.env.DATABASE_URL);
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+
   ssl: {
     rejectUnauthorized: false,
   },
+});
+
+pool.on("connect", () => {
+  console.log("Conectado a PostgreSQL");
+});
+
+pool.on("error", (err) => {
+  console.error("Error PostgreSQL:", err);
 });
--- backend/src/index.js
+++ backend/src/index.js
@@ -4,13 +4,13 @@ import { pool } from "./db.js";
 import obrasRoutes from "./routes/obras.routes.js";
 import tablerosRoutes from "./routes/tableros.routes.js";
 import circuitosRoutes from "./routes/circuitos.routes.js";
-import terminacionesRoutes from "./routes/terminaciones.routes.js";
+/*import terminacionesRoutes from "./routes/terminaciones.routes.js";
 import puestaATierraRoutes from "./routes/puestaATierra.routes.js";
 import tablerosMaterialesRoutes from "./routes/tablerosMateriales.routes.js";
 import luminariasRoutes from "./routes/luminarias.routes.js";
 import bandejasRoutes from "./routes/bandejas.routes.js";
 import ductosRoutes from "./routes/ductos.routes.js";
-import porterosRoutes from "./routes/porteros.routes.js";
+import porterosRoutes from "./routes/porteros.routes.js";*/
 import catalogosRoutes from "./routes/catalogos.routes.js";
 
 const app = express();
@@ -40,13 +40,13 @@ app.get("/test-db", async (req, res) => {
 app.use("/api/obras", obrasRoutes);
 app.use("/api/tableros", tablerosRoutes);
 app.use("/api/circuitos", circuitosRoutes);
-app.use("/api/terminaciones", terminacionesRoutes);
+/*app.use("/api/terminaciones", terminacionesRoutes);
 app.use("/api/puesta-a-tierra", puestaATierraRoutes);
 app.use("/api/tableros-materiales", tablerosMaterialesRoutes);
 app.use("/api/luminarias", luminariasRoutes);
 app.use("/api/bandejas", bandejasRoutes);
 app.use("/api/ductos", ductosRoutes);
-app.use("/api/porteros", porterosRoutes);
+app.use("/api/porteros", porterosRoutes);*/
 app.use("/api/catalogos", catalogosRoutes);
 
 app.listen(PORT, () => {
